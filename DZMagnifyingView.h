9c4468d0468512849cb4fdced4429beef28a0ce3
Dan Zimmerman <daniel.zimmerman@me.com>
2014-02-14 18:30:10 -0600

    Added comments to hdr

--- DZMagnifyingView.h
+++ DZMagnifyingView.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
-
+/*
+ * @description A UIView subclass that acts like a magnifying glass.
+ *
+ * @discussion You can initialize this class like an ordinary view with one caveat: the width and height should be the same, the behavior is unknown otherwise.
+ *
+ * @availabilty This is the one and only class here so it should always be available.
+ *
+ */
 @interface DZMagnifyingView : UIView {
     UIImage *fullImage;
     CGAffineTransform rotationTransform;
@@ -16,12 +23,30 @@
     CAShapeLayer *borderLayer;
     BOOL queueHide;
 }
-
+/*
+ * @description The radius of what you want to magnify. Probably should be smaller than the size of the magnifying view if you want to magnify something
+ */
 @property (nonatomic) CGFloat closeupRadius;
+/*
+ * @description The center of what you want to magnify.
+ */
 @property (nonatomic) CGPoint closeupCenter;
+/*
+ * @description The actual view you want to magnify.
+ *
+ * @discussion If you want to update the cache of the view (something has changed on screen since you last set the targetView), just set this again with the same view. The device doesn't perform well automatically recaching everyframe or so, or even whenever you change the center of this view, so we require you to tell us to cache it instead.
+ */
 @property (nonatomic) UIView *targetView;
 
+/*
+ * @description Don't call -addSubview: with this view as the arguement manually on `targetView`, call this instead once you have set [closeupRadius](#closeupRadius), [closeupCenter](#closeupCenter) and [targetView](#targetView).
+ *
+ * @discussion You should set the center of the magnifying glass before calling this (or set the frame, just move it somewhere).
+ */
 - (void)show;
+/*
+ * @description Don't call -removeFromSuperview manually, call this instead.
+ */
 - (void)hide;
 
 @end
